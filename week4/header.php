b4b4fcd0814a9e9d58849976935c1155216be47d
Alan Auckland <alanauckland86@gmail.com>
2014-10-01 07:34:22 +0100

    started
    week 4’s lessons

---
+++ week4/header.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE>
+	<html>
+
+		<head>
+			<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+			<title> <?php echo $pageTitle ?></title>
+			<link rel="stylesheet" type="text/css" href="style.css">
+		</head>
+
+		<body>
+			<h1><?php echo $pageHeading ?></h1>
+
+
